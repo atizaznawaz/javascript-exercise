f47dd88bc974db5260ddb840ba8627fcd22aa097
atizaznawaz <atizaznawaz@gmail.com>
2020-06-14 19:53:37 +0500

    uploading exercise 1-6

---
+++ app.js
@@ -0,0 +1,265 @@
+// alert("Task 1 starting");
+
+// alert("Welcome in our Website");
+
+// alert("Task 2 starting");
+
+// alert("Error! Please enter your valid password");
+
+// alert("Task 3 starting");
+
+// alert("Welcome to Js Land \n Happy Coding");
+
+// alert("Task 4 starting");
+
+// alert("Welcome to Js Land ");
+
+// alert("Happy Coding");
+
+// var username;
+// var myName = "Atizaz Nawaz";
+// var message = "Hello World";
+
+// alert(message);
+
+// var stdName = "Atizaz Nawaz";
+// var stdAge = "22";
+// var stdCertification = "Certified Mobile Applicationn Development";
+
+// alert(stdName);
+// alert(stdAge + " Year Old");
+// alert(stdCertification);
+
+// var food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(food);
+
+// var email = "example@example.com";
+// alert("My email address is " + email);
+
+// var book = "A Smarter Way To Learn JavaScript";
+// alert("I am trying to larn book  " + book);
+
+// var message1 = "Yeah I can write HTML Content through JavaScript";
+// document.write(message1);
+
+// var line = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(line);
+
+// var age = 15;
+// alert("i am " + age + " Year Old");
+
+// var n = 15;
+// alert("You have visited the page " + n + " times");
+
+// var birthYear = 1998;
+// var type = typeof birthYear;
+// document.write(
+//   "My birth Year is " +
+//     birthYear +
+//     "\n and data type of this variable is " +
+//     type
+// );
+
+// var visitorName = "Atizaz";
+// var store = "CSD shopping store";
+// var product = "shoes";
+
+// document.write(
+//   visitorName + " ordered 3 pair of " + product + " from " + store
+// );
+
+// var name = "atizaz";
+// klass = "5";
+// section = "3b";
+
+// var Name = "Atizaz";
+// var Name1 = "aaaa";
+// var _start = "aa";
+// var st_3='weather';
+// var st_qq="what";
+
+// var 123="dwwc";
+// var 1awwd="";
+// var @ya="";
+// var at@ a="";
+// var -atizaz="";
+
+// var a = 10;
+// var b = 19;
+// var c = a + b;
+// document.write("The Sum of " + a + " and " + b + " is equal to " + c);
+
+// var a = 10;
+// var b = 19;
+// var c = a * b;
+// document.write("The Multiplication of " + a + " and " + b + " is equal to " + c);
+
+// var a = 10;
+// var b = 19;
+// var c = a / b;
+// document.write("The division of " + a + " and " + b + " is equal to " + c);
+
+// var a = 10;
+// var b = 19;
+// var c = a - b;
+// document.write("The Subtraction of " + a + " and " + b + " is equal to " + c);
+
+// var number = 5;
+// document.write("the variable decleration is " + number + "<br>");
+
+// document.write("the initial value is " + number + "<br>");
+
+// var number = ++number;
+
+// document.write("the value after increment of 1 is" + number + "<br>");
+
+// var number = 7 + number;
+
+// document.write("the value after increment of 7 is" + number + "<br>");
+
+// var number = --number;
+
+// document.write("the value after decrement of 1 is" + number + "<br>");
+
+// var division = number % 3;
+
+// document.write("the remainder after division with 3 is " + division + "<br>");
+
+// var ticket = 600;
+// var seats = 5;
+// var total = ticket * seats;
+
+// document.write(
+//   "The total cost to buy " + seats + " tickts to a movie is " + total +" PKR"
+// );
+
+// var number = 4;
+
+// document.write("Table of " + number + "<br><br>");
+
+// for (var i = 1; i <= 10; i++) {
+//   document.write(number + " x " + i + " = " + number * i + "<br>");
+// }
+
+// var C = 25;
+// var F = 70;
+
+// var q = F - 32;
+// var e = 5 / 9;
+
+// var c = q * e;
+
+// var f = C * e + 32;
+
+// document.write(
+//   "The Conversion of " + C + " Celcius into Ferhenite is " + c + " F" + "<br>"
+// );
+// document.write(
+//   "The Conversion of " + F + " Ferhenite into Celcius is " + f + " C" + "<br>"
+// );
+
+// var item1Price = 650;
+// var item2Price = 100;
+// var shipCharges = 100;
+
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+
+// var total =
+//   item1Price * quantityOfItem1 + item2Price * quantityOfItem2 + shipCharges;
+
+// document.write("Price of item 1 is :" + item1Price + "<br>");
+// document.write("Quantity of item 1 is :" + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is :" + item2Price + "<br>");
+// document.write("Quantity of item 2 is :" + quantityOfItem2 + "<br>");
+// document.write("Shipping charges are :" + shipCharges + "<br><br>");
+// document.write("Total cost of order  is :" + total + "<br>");
+
+// var obtainmarks = 804;
+// var totalmarks = 980;
+// var percentage = (obtainmarks * 100) / totalmarks;
+
+// document.write("Total Marks is: " + totalmarks + " <br>");
+// document.write("Obtain Marks is: " + obtainmarks + " <br>");
+// document.write("Percentage is: " + percentage + " % <br>");
+
+// var dollar = 104.8;
+// var saudiRiyal = 28;
+
+// var total = 10 * dollar + 25 * saudiRiyal;
+
+// document.write("Total Currency is PKR is: " + total);
+
+// var number = 10;
+
+// var sum = 10 + 5;
+
+// var mul = sum * 10;
+
+// var div = mul / 2;
+
+// document.write("The number is: " + number + "<br>");
+// document.write("Add 5 with Number The result is: " + sum + "<br>");
+// document.write("Multiply 10 with that number The result is: " + mul + "<br>");
+// document.write("Divide number by 2 The result is: " + div + "<br>");
+
+// var year = 2020;
+// var birthyear = 1998;
+
+// var guess = year - birthyear;
+// document.write("Current Year is: " + year + "<br>");
+// document.write("Birth Year is: " + birthyear + "<br>");
+// document.write("The Guess age is: " + guess + " year old");
+
+// var radius = 20;
+// var circumference = 2 * 3.142 * radius;
+// var area = 3.142 * radius * 2;
+
+// document.write("Circle Radius is: " + radius + "<br>");
+// document.write("Circumference of circle is: " + circumference + "<br>");
+// document.write("Area of Circle is: " + area + "<br>");
+
+// var snack = "lays";
+// var age = 22;
+// var maxage = 65;
+// var snackamount = 3;
+
+// var totalsnacks = maxage * 365 * snackamount;
+
+// document.write("Favourite Snack: " + snack + "<br>");
+// document.write("Age is : " + age + "<br>");
+// document.write("Maximum age is: " + maxage + "<br>");
+// document.write("Snacks eat perday: " + snackamount + "<br>");
+
+// document.write(
+//   "You will need " +
+//     totalsnacks +
+//     "\t" +
+//     snack +
+//     " to last you until the ripe oldage " +
+//     maxage
+// );
+
+// var a = 10;
+// document.write("RESULT<br>");
+// document.write("the value of a is: " + a + "<br>");
+// document.write("----------------------------------- <br>");
+
+// var a = ++a;
+// document.write("the value of ++a is: " + a + "<br>");
+// document.write("Now the value of ++a is: " + a + "<br><br>");
+
+// var a = a++;
+// document.write("the value of a++ is: " + a + "<br>");
+// var a = ++a;
+// document.write("Now the value of a is: " + a + "<br><br>");
+
+// var a = --a;
+// document.write("the value of --a is: " + a + "<br>");
+// var a = a;
+// document.write("Now the value of a is: " + a + "<br><br>");
+
+// var a = a--;
+// document.write("the value of a-- is: " + a + "<br>");
+// var a = --a;
+// document.write("Now the value of a is: " + a + "<br><br>");
